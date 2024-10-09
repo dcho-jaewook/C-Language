d2b50230941303df5ccdcaf7f39bb04d4855c58b
dcho-jaewook <fermatjw@gmail.com>
2024-10-08 18:18:21 -0700

    Initial Commit

---
+++ C Language/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C Language
+//
+//  Created by jaewook on 10/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
